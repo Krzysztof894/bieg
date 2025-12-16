25f176dd9ae93d45dd1ba08486c5c0d6db8da01c
Krzysztof894 <kk89442@stud.uws.edu.pl>
2025-12-16 19:38:06 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Strona Bieg Studenta 2025 załadowana pomyślnie.');
+    console.log('Strona Bieg Studenta załadowana pomyślnie.');
 
-    // Pobierz wszystkie przyciski, które mają klasę 'placeholder-link'
-    // (Dodałem tę klasę w HTML do przycisków z href="#")
     const placeholderLinks = document.querySelectorAll('.placeholder-link');
 
     placeholderLinks.forEach(link => {
         link.addEventListener('click', (event) => {
-            // Zatrzymaj domyślne działanie (skok do góry strony)
             event.preventDefault();
-            
-            // Wyświetl komunikat dla użytkownika
             alert('Ta funkcjonalność (formularz/pobieranie) będzie dostępna wkrótce!');
         });
     });
